5294bcd1af50c0d5d7c6a0ff837b0557ebf97d28
Josué Martínez Carrasco <josue@jmcsoluciones.com>
2024-11-16 10:50:42 -0600

    Se crea un functional component con integración del less

---
+++ 03-react/03-counter-app-vite/src/ComponentTest.jsx
@@ -0,0 +1,24 @@
+// import React from 'react'
+import PropTypes from "prop-types"
+const ComponentTest = ({name, date, content}) => {
+  return (
+    <>
+    <div className="box">
+        <div className="box__title">
+            <h2>Nombre: {name}</h2>
+            <p>Fecha: {date}</p>
+        </div>
+        <div className="box__content">
+            <p>{content}</p>
+        </div>
+    </div>
+    </>
+  )
+}
+ComponentTest.propTypes = {
+    name: PropTypes.string.isRequired,
+    date: PropTypes.string.isRequired,
+    content: PropTypes.string.isRequired
+}
+
+export default ComponentTest
--- 03-react/03-counter-app-vite/src/main.jsx
+++ 03-react/03-counter-app-vite/src/main.jsx
@@ -5,10 +5,17 @@ import { FirstApp } from './FirstApp';
 // import { HellowWorldApp} from './HelloWorldApp'
 import './styles.css';
 import { CounterApp } from './CounterApp';
+import ComponentTest from './ComponentTest';
+
+const nombre = prompt("Ingrese su nombre: ");
+const fecha = Date();
+const contenido = "lorem ipsum"
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         {/* <FirstApp title="hola" subtitle="16 de Noviembre"/> */}
-        <CounterApp value = {20358}/>
+        {/* <CounterApp value = {20358}/> */}
+        <ComponentTest name={nombre} date={fecha} content={contenido}></ComponentTest>
     </React.StrictMode>
 );
