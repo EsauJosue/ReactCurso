8d8c79042f6d74c556782db8d04a7fcfb5242cca
Josué Martínez Carrasco <josue@jmcsoluciones.com>
2024-11-17 16:52:26 -0600

    Se implementa por primera vez un useState, primer hook

--- 03-react/03-counter-app-vite/src/main.jsx
+++ 03-react/03-counter-app-vite/src/main.jsx
@@ -7,15 +7,11 @@ import './styles.css';
 import { CounterApp } from './CounterApp';
 import ComponentTest from './ComponentTest';
 
-const nombre = prompt("Ingrese su nombre: ");
-const fecha = Date();
-const contenido = "lorem ipsum"
-
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         {/* <FirstApp title="hola" subtitle="16 de Noviembre"/> */}
-        {/* <CounterApp value = {20358}/> */}
-        <ComponentTest name={nombre} date={fecha} content={contenido}></ComponentTest>
+        <CounterApp value = {1}/>
+        {/* <ComponentTest name={nombre} date={fecha} content={contenido}></ComponentTest> */}
     </React.StrictMode>
 );
